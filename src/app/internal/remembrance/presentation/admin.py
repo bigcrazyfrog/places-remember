f15a8b4f3dc7224b0805f060107cf575d71ad391
Biven1 <cibinnikita@gmail.com>
2023-05-30 18:32:25 +0500

    register admin place

--- src/app/internal/remembrance/presentation/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from app.internal.remembrance.db.models import Place
-
-
-@admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
-    pass
