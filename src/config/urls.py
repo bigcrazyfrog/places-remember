5a1735703dcfb5d32125c70bd76477d47ec0c24d
Biven1 <cibinnikita@gmail.com>
2023-05-30 18:29:00 +0500

    add path

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+
 from django.urls import include, path
+
+from app.internal.app import urls
+
+app_name = "app"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include(urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
